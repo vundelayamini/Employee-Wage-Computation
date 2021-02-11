ff04321e7e455a93c6341d5a874ab39b4845d7f1
yamini <vundelayamini@gmail.com>
2021-02-11 07:38:37 -0800

    Added: employee parttime

--- empWage.sh
+++ empWage.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -x
 
 #constant
-EMP_RATE_PER_HR=20
-WORK_HR=8
+EMP_RATE_PER_HR=20;
 
 #variable
-empCheck=$((RANDOM%2))
-ispresent=1
+ispartTime=1;
+isFullTime=2;
+empcheck=$((RANDOM%3))
 
 case $empCheck in
-   $ispresent)
-       salary=$(($WORK_HR*$EMP_RATE_PER_HR))
-         echo $salary
-        ;;
-       *)
-       echo Employee is absent
-        ;;
-esac
+      $isFullTime)
+          empHrs=8
+         ;;
+       $isPartTime)
+          empHrs=4
+          ;;
+        *)
+           echo Employee is present
+   esac
+salary=$(($empHrs*$EMP_RATE_PER_HR))
+echo $salary
