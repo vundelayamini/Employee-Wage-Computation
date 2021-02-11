a4ffa533b92cedf1ce485464e1cad8f355140bba
yamini <vundelayamini@gmail.com>
2021-02-11 08:19:31 -0800

    Added: calculate Wages for A Month

--- empWage.sh
+++ empWage.sh
@@ -1,19 +1,26 @@
 #!/bin/bash -x
 
-EMP_RATE_PER_HR=20;
 isPartTime=1;
 isFullTime=2;
-empCheck=$(( RANDOM%3 ));
+totalSalary=0;
+numofWorkingDays=20;
+EMP_RATE_PER_HR=20;
 
-case $empCheck in
-      $isFullTime)
-          empHrs=8
-            ;;
-       $isPartTime)
-          empHrs=4
-            ;;
-        *)
-          empHrs=0
-            ;;
+for (( day=1; day<=$numofWorkingDays; day++ ))
+do
+   empCheck=$(( RANDOM%3 ));
+       case $empCheck in
+                 $isFullTime)
+                     empHrs=8
+                      ;;
+                 $isPartTime)
+                    empHrs=4
+                     ;;
+                 *)
+                  empHrs=0
+                   ;;
    esac
-salary=$(($empHrs*$EMP_RATE_PER_HR));
+
+   salary=$(($empHrs*$EMP_RATE_PER_HR));
+   totalsalary=$(($totalSalary+$salary));
+done
