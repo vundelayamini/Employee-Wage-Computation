b85906ad3a37dd91aeff42882d4c565c1088bfd0
yamini <vundelayamini@gmail.com>
2021-02-11 09:19:19 -0800

    Added: toGetWorkHours

--- empWage.sh
+++ empWage.sh
@@ -10,25 +10,31 @@ MAX_HRS_IN_MONTH=100;
 #VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
+function getWorkHrs() {
+    local empCheck=$1
+     case $empCheck in
+      $IS_FULL_TIME)
+          empHrs=8
+            ;;
+      $IS_PART_TIME)
+         empHrs=4
+            ;;
+         *)
+            empHrs=0
+             ;;
+     esac
+       echo $empHrs
+}
 
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
-  ((totalWorkingDays++))
-  empCheck=$(( RANDOM%3 ))
-  case $empCheck in
-       $IS_FULL_TIME)
-          empHrs=8
-           ;;
-        $IS_PART_TIME)
-           empHrs=4
-            ;;
-        *)
-           empHrs=0
-             ;;
-     esac
-totalEmpHrs=$(($totalEmpHrs+$empHrs))
+    ((totalWorkingDays++))
+      empCheck=$(( RANDOM%3 ))
+      empHrs="$(getWorkHrs $empCheck )"
+      totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
-totalsalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+
+totalsalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
 
 
 
