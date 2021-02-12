ee997b5e059715823ab0f56ba42c57fb17227c6f
yamini <vundelayamini@gmail.com>
2021-02-11 22:29:24 -0800

    Added : empDictionary

--- empWage.sh
+++ empWage.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -x
 
-#CONSTANTS FOR THE PROGRAM
+# CONSTANTS FOR THE PROGRAM
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 MAX_HRS_IN_MONTH=4;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-#VARIABLES
+# VARIABLES
 totalEmpHr=0;
 totalWorkingDays=0;
+
+declare -A dailyWage;
 
 function getWorkHrs() {
      local $empCheck=$1
@@ -28,20 +30,20 @@ function getWorkHrs() {
   echo $empHrs
 }
 function getEmpWage() {
-   local $empHrs=$1
-    echo $(($empHrs+$EMP_RATE_PER_HR))
+   local $empHr=$1
+    echo $(($empHr*$EMP_RATE_PER_HR))
 }
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
       ((totalWorkingDays++))
-      empCheck=$(( RANDOM%3 ))
+      empCheck=$((RANDOM%3));
       empHrs="$(getWorkHrs $empCheck )"
       totalEmpHrs=$(($totalEmpHrs+$empHrs))
-      dailyWage[$totalWoringDays]="$( getEmpWage $empHrs )"
+      dailyWage[" Day $totalWoringDays"]="$( getEmpWage $empHrs )"
 done
 totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
 echo ${dilyWage[@]}
-
+echo ${!dailywage[@]}
 
 
 
